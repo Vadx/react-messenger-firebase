3927c59b7b80e1d1c8ec270b82bc50e6bc26de52
Vadx <lv_km@ukr.net>
2021-03-15 17:06:54 +0200

    Part 2

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { Context } from '../index'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { Avatar, Box, Button, Container, Grid } from '@material-ui/core'
+import { Avatar, Box, Button, Container, Grid, Card, CardHeader, CardContent, Typography } from '@material-ui/core'
 import TextField from '@material-ui/core/TextField'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 import Loader from './Loader'
@@ -39,20 +39,29 @@ const Chat = () => {
       >
         <Box style={{ width: '80%', height: '60vh', border: '1px solid gray', overflowY: 'auto' }}>
           {messages.map(message =>
-            <Box style={{
+            <Card style={{
               margin: 10,
-              border: user.uid === message.uid ? '2px solid green' : '2px dashed red',
+              border: user.uid === message.uid ? '1px solid green' : '1px dashed red',
               marginLeft: user.uid === message.uid ? 'auto' : '10px',
               width: 'fit-content',
               padding: 5
             }}
             >
-              <Grid container>
-                <Avatar src={message.photoURL} />
-                <div>{message.displayName}</div>
-              </Grid>
-              <div>{message.text}</div>
-            </Box>
+              <CardHeader
+                avatar={
+                  <Avatar src={message.photoURL} />
+                }
+                title={message.displayName}
+                // subheader={message.createdAt}
+              > 
+              </CardHeader>
+              <CardContent>
+                <Typography variant='body2' color='textSecondary' component='p'>
+                  {message.text}
+                </Typography>
+                {console.log(message.createdAt.seconds)}
+              </CardContent>
+            </Card>
           )}
         </Box>
         <Grid
